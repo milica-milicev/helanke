46f8de529894104d6f77b12f32713e046abd5d81
milica-milicev <142879244+milica-milicev@users.noreply.github.com>
2024-11-05 13:00:16 +0100

    banner box updated

--- template-views/blocks/banner-box/banner-box.php
+++ template-views/blocks/banner-box/banner-box.php
@@ -1,13 +1,13 @@
 <div class="banner-box">
     <div class="container">
-        <div class="banner-box__wrap">
+        <a href="javascript:;" class="banner-box__wrap">
             <div class="banner-box__img">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/_demo/banner-box.webp" alt="">
             </div>
             <div class="banner-box__content">
                 <h2 class="banner-box__title">Otkrij svoj novi omiljeni top! -20%</h2>
-                <a class="btn btn--white" href="javascript:;">Saznajte više</a>
+                <span class="btn btn--white">Saznajte više</span>
             </div>
-        </div>
+        </a>
     </div>
 </div>
