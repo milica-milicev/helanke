fb4e0bd1d844c93b9d0e279247a9f4381501227b
milica-milicev <142879244+milica-milicev@users.noreply.github.com>
2024-11-08 13:12:23 +0100

    contact updated

--- template-views/blocks/banner/banner-blog.php
+++ template-views/blocks/banner/banner-blog.php
@@ -1,6 +1,6 @@
 <div class="banner" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/_demo/blog-banner.webp');">
     <div class="container">
-        <div class="banner-content">
+        <div class="banner__content">
             <h1 class="banner__title">Blog</h1>
         </div>
     </div>
--- template-views/blocks/banner/banner.php
+++ template-views/blocks/banner/banner.php
@@ -1,6 +1,6 @@
 <div class="banner" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/_demo/contact.webp');">
     <div class="container">
-        <div class="banner-content">
+        <div class="banner__content">
             <h1 class="banner__title">Kontaktirajte nas</h1>
         </div>
     </div>
