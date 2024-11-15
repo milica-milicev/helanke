a1c617a3865f1bfa996f9dda85105c4e540b0453
milica-milicev <142879244+milica-milicev@users.noreply.github.com>
2024-11-15 15:45:27 +0100

    categories

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -20,72 +20,82 @@ get_header( 'shop' );
 do_action( 'woocommerce_before_main_content' );
 ?>
 
-<header class="woocommerce-products-header">
-    <?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-        <h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-    <?php endif; ?>
+<div class="banner" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/_demo/blog-banner.webp');">
+    <div class="container">
+        <div class="banner__content">
+            <h1 class="banner__title"><?php woocommerce_page_title(); ?></h1>
+        </div>
+    </div>
+</div>
 
-    <?php
-    /**
-     * Hook: woocommerce_archive_description.
-     *
-     * @hooked woocommerce_taxonomy_archive_description - 10
-     * @hooked woocommerce_product_archive_description - 10
-     */
-    do_action( 'woocommerce_archive_description' );
+<?php
+/**
+ * Hook: woocommerce_archive_description.
+ *
+ * @hooked woocommerce_taxonomy_archive_description - 10
+ * @hooked woocommerce_product_archive_description - 10
+ */
+do_action( 'woocommerce_archive_description' );
 
-    // Display subcategories if on a category archive
-    if ( is_product_category() ) {
-        $term = get_queried_object();
-        $args = array(
-            'parent'       => $term->term_id,
-            'taxonomy'     => 'product_cat',
-            'hide_empty'   => false, // Change to true to hide empty categories
-        );
+// Display subcategories if on a category archive
+if ( is_product_category() ) :
+    $term = get_queried_object();
+    $args = array(
+        'parent'       => $term->term_id,
+        'taxonomy'     => 'product_cat',
+        'hide_empty'   => false, // Change to true to hide empty categories
+    );
 
-        $subcategories = get_terms( $args );
+    $subcategories = get_terms( $args );
 
-        if ( ! empty( $subcategories ) && ! is_wp_error( $subcategories ) ) {
-            echo '<div class="subcategory-list">';
-            echo '<h2>Subcategories</h2>';
-            echo '<ul class="subcategories">';
+    if ( ! empty( $subcategories ) && ! is_wp_error( $subcategories ) ) : ?>
 
-            foreach ( $subcategories as $subcategory ) {
-                echo '<li>';
-                echo '<a href="' . esc_url( get_term_link( $subcategory ) ) . '">';
-                echo esc_html( $subcategory->name );
-                echo '</a>';
-                echo '</li>';
-            }
-
-            echo '</ul>';
-            echo '</div>';
-        }
-    }
-    ?>
-</header>
+        <div class="categories categories--listing">
+            <div class="container">
+                <div class="categories__row">
+                    <?php foreach ( $subcategories as $subcategory ) : ?>
+                        <div class="categories__item">
+                            <a href="<?php echo esc_url( get_term_link( $subcategory ) ); ?>">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/_demo/trudnice.jpg" alt="">
+                                <span><?php echo esc_html( $subcategory->name ); ?></span>
+                            </a>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </div>
+    <?php endif;
+endif; ?>
 
 <?php
 // Continue with the existing product loop and other content
-if ( woocommerce_product_loop() ) {
-    do_action( 'woocommerce_before_shop_loop' );
-    woocommerce_product_loop_start();
+if ( woocommerce_product_loop() ) : ?>
+    <div class="products-list">
+        <div class="container">
+            <?php
+                do_action( 'woocommerce_before_shop_loop' );
 
-    if ( wc_get_loop_prop( 'total' ) ) {
-        while ( have_posts() ) {
-            the_post();
-            do_action( 'woocommerce_shop_loop' );
-            wc_get_template_part( 'content', 'product' );
-        }
-    }
-
-    woocommerce_product_loop_end();
+                if ( wc_get_loop_prop( 'total' ) ) : ?>
+                    <div class="products-list__row">
+                        <?php while ( have_posts() ) : ?>
+                            <div class="products-list__item">
+                                <?php
+                                    the_post();
+                                    do_action( 'woocommerce_shop_loop' );
+                                    wc_get_template_part( 'content', 'product' ); 
+                                ?>
+                            </div>
+                        <?php endwhile; ?>
+                    </div>
+               <?php endif; ?>
+        </div>
+    </div>
+    <?php
     do_action( 'woocommerce_after_shop_loop' );
-} else {
+else :
     do_action( 'woocommerce_no_products_found' );
-}
+endif;
 
 do_action( 'woocommerce_after_main_content' );
-//do_action( 'woocommerce_sidebar' );
 
 get_footer( 'shop' );
